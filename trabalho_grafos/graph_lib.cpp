a3a7d2f88c2357c10851b8d2787d9765d3f85542
afabisa <fabianesa12@gmail.com>
2025-09-23 20:53:50 -0300

    Recebendo o grafo e gerando a saída com informações básicas

--- trabalho_grafos/graph_lib.cpp
+++ trabalho_grafos/graph_lib.cpp
@@ -37,3 +37,96 @@ vector<vector<int>> txt_to_adjacency_vector(const string& nome_arquivo, string n
     }
 
     arquivo.close();
+
+    // Ordenar os graus
+    sort(degrees.begin() + 1, degrees.end()); 
+
+    // Encontrar grau máximo, mínimo e calcular estatísticas
+    int maxDegree = degrees[numVertices];
+    int minDegree = degrees[1];
+    double averageDegree = static_cast<double>(degreeSum) / numVertices;
+    double medianDegree; //declarando variável para mediana
+
+    // Calcular a mediana do grau
+    if (numVertices % 2 == 0) {
+        medianDegree = (degrees[numVertices / 2] + degrees[(numVertices / 2) + 1]) / 2.0;
+    } else {
+        medianDegree = degrees[(numVertices + 1) / 2];
+    }
+
+    // Função para calcular componentes conexas
+    vector<vector<int>> componentes; //vetor de componentes
+    vector<int> tamanhos; //vetor com o tamanho de cada componente
+    int quant_componentes = 0; //número de componentes
+    vector<bool> visitados(graph.size(), false); //vetor que indica se o nó foi visitado ou não. 
+    queue<int> fila;
+    int iterador = 0; 
+
+    for (int node = 1; node < graph.size(); node++) { //percorrendo os vértices do grafo
+        if (!visitados[node]) { //verifica se o vértice não foi visitado
+            quant_componentes++;
+            componentes.push_back(vector<int>()); //adiciona um vetor vazio no vetor de componentes
+            tamanhos.push_back(0); //adiciona um valor no vetor com o tamanho das componentes. 
+            fila.push(node); //adiciona o vértice na fila 
+            visitados[node] = true; //marca o vértice como visitado
+            componentes[iterador].push_back(node); //adiciona o vértice no vetor da componente
+            tamanhos[iterador]++;//incrementa o tamanho da componente atual (em 1)
+
+            while (!fila.empty()) { 
+                //obtém e remove o vértice da fila
+                int atual = fila.front(); 
+                fila.pop(); 
+
+                for (int vizinho : graph[atual]) { 
+                    //para cada vizinho do vértice atual, verificar se foi visitado. 
+                    // Se não, colocar o vizinho na fila. 
+                    // Alterar valor no vetor de visitados para 1 (true)
+                    // Adicionar vizinho na sua componente
+                    // Incrementar o tamanho da componente no vetor de tamanhos. 
+                    if (!visitados[vizinho]) {
+                        fila.push(vizinho);
+                        visitados[vizinho] = true;
+                        componentes[iterador].push_back(vizinho);
+                        tamanhos[iterador]++;
+                    }
+                }
+            }
+            iterador++;
+        }
+    }
+    //ordena componentes em ordem decrescente
+    sort(componentes.begin(), componentes.end(), [](const vector<int>& a, const vector<int>& b) {
+        return a.size() > b.size();
+    }); 
+    sort(tamanhos.begin(), tamanhos.end()); 
+    reverse(tamanhos.begin(), tamanhos.end()); //coloca vetor de tamanhos em ordem decrescente
+
+    // Gravar os resultados no arquivo de saída no modo append (adiciona ao final do arquivo)
+    ofstream arquivo_de_saida(nome_do_arquivo_de_saida_principal, std::ios::app);
+
+    if (arquivo_de_saida.is_open()) {
+        arquivo_de_saida << "Número de vértices: " << numVertices << endl;
+        arquivo_de_saida << "Número de arestas: " << numArestas << endl;
+        arquivo_de_saida << "Grau mínimo: " << minDegree << endl;
+        arquivo_de_saida << "Grau máximo: " << maxDegree << endl;
+        arquivo_de_saida << "Grau médio: " << averageDegree << endl;
+        arquivo_de_saida << "Mediana de grau: " << medianDegree << '\n' << endl;
+
+        // Adicionar os resultados das componentes conexas
+        arquivo_de_saida << "Número de componentes conexas: " << quant_componentes << endl << endl;
+        for (int j = 0; j < tamanhos.size(); j++) {
+            arquivo_de_saida << "Componente " << j + 1 << " tem " << tamanhos[j] << " vértices, e eles são: " << endl;
+            for (int i = 0; i < componentes[j].size(); i++) {
+                arquivo_de_saida << componentes[j][i] << " ";
+            }
+            arquivo_de_saida << endl << endl << endl;
+        }
+        //Salva o arquivo e sinaliza no terminal. 
+        arquivo_de_saida.close();
+        cout << "Resultados gravados no arquivo " << nome_do_arquivo_de_saida_principal << endl;
+    } else {
+        cout << "Erro ao abrir o arquivo de resultados!" << endl;
+    }
+
+        return graph;
+}
